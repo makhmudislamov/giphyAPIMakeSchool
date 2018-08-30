d222edcce5fe7b3ee419a4d3edf8e57a956ab376
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-08-30 16:12:41 -0700

    completed

--- app.js
+++ app.js
@@ -1,41 +1,25 @@
+// variables
 var express = require('express');
 var app = express();
-
-// hanldebars
+var giphy = require('giphy-api')();
 var exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-// First route
-// if I do: app.get('/hello-world', it doesn't work
-// app.get('/', function (req, res) {
-//     res.send('Hello World');
-// });
-
-
-// Second Route
-// updating my route
-// if I do: app.get('/hello-gif'...  NOT WORKING
-// app.get('/', function (req, res) {
-//     var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
-//     res.render('hello-gif', { gifUrl: gifUrl })
-// });
-
-
-// Third Route
-// If I do: app.get('/greetings/:name',... NOT WORKING
-// app.get('/', function (req, res) {
-//     var name = req.params.name;
-//     res.render('greetings', { name: name });
-// });
-
-// Fourth Route - Home Page
+// root
 app.get('/', function (req, res) {
-    console.log(req.query)
-    res.render('home')
+    // Thanks Ikey ))
+    let searchedGiphy = req.query.term || "lamborghini"
+    giphy.search(searchedGiphy, function (err, response) {
+        res.render('home', { gifs: response.data })
+    });
 });
 
+// CSS
+app.use(express.static('public'));
+
+// server
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
